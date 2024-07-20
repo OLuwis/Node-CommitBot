279f458787bd958c33d12adce5f1fdb68002a103
Luis Miguel <oluismrs@gmail.com>
2024-07-20 19:32:57 -0300

    create script

---
+++ app.ts
@@ -0,0 +1,38 @@
+import { Octokit } from "octokit";
+
+const auth = process.env["TOKEN"]
+
+const owner = "oluwis"
+const repo = "node-commitbot"
+const path = "output.txt"
+
+const date = new Date().toUTCString().replace("GMT", "UTC")
+const message = `Commit: ${date}`
+
+const octokit = new Octokit({
+  auth: auth
+})
+
+const file = await octokit.rest.repos.getContent({
+  owner: owner,
+  repo: repo,
+  path: path
+})
+
+if (file && ("sha" && "content" in file.data)) {
+  const sha = file.data.sha
+
+  const fileContent = Buffer.from(file.data.content, "base64").toString("ascii")
+  const content = Buffer.from(`${message}\n${fileContent}`).toString("base64")
+
+  await octokit.rest.repos.createOrUpdateFileContents({
+    owner: owner,
+    repo: repo,
+    path: path,
+    sha: sha,
+    content: content,
+    message: message
+  })
+
+  console.log(message)
+}
